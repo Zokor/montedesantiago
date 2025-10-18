0bf3000d1ae8cf1692a16000fb594d40bfa65691
Zokor <bflgomes@gmail.com>
2025-10-18 16:03:41 +0100

    chore(admin): add Inertia import to PageController
    Add use Inertia\Inertia; to enable Inertia responses in the admin PageController and prevent undefined class errors when rendering Inertia views.

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -16,6 +16,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class PageController extends Controller
 {
